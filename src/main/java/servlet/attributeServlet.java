071d9cda0358e87ee72e9f4c3ffee48b43f1f0cc
Christopher Park <basket123rocks@gmail.com>
2020-04-21 19:11:03 -0400

    new mesg

--- src/main/java/servlet/attributeServlet.java
+++ src/main/java/servlet/attributeServlet.java
@@ -69,7 +69,7 @@ public void doGet (HttpServletRequest request, HttpServletResponse response)
    out.println(" <br><input type=\"checkbox\" name=\"attrib_remove\">Remove");
    out.println(" <input type=\"submit\" name=\"update\" value=\"Update\">");
    out.println("</form>");
-      String lifeCycleURL = "";
+      String lifeCycleURL = "/offutt/servlet/sessionLifeCycle";
       out.print  ("<br><br><a href=\"" + lifeCycleURL + "?action=invalidate\">");
       out.println("Invalidate the session</a>");
       out.print  ("<br><a href=\"" + lifeCycleURL + "\">");
